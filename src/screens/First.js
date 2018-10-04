c73a8cc9678e555b438fd0fff2559ef5fb53fc31
Fernando <ferdelamad@gmail.com>
2018-10-03 19:55:33 -0700

    Added logo animation test

--- src/screens/First.js
+++ src/screens/First.js
@@ -1,24 +1,51 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { Text, StyleSheet } from "react-native";
 import { Button } from "../common/Button";
+import { Image, View } from "react-native-animatable";
 
-export const First = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Screen 1</Text>
-      <Button onPress={() => navigation.navigate("Second")}>
-        Go to screen 2
-      </Button>
-    </View>
-  );
-};
+import logo from "../assets/logo-test.png";
+import metrics from "../assets/config";
+const IMAGE_WIDTH = metrics.DEVICE_WIDTH * 0.8;
+
+export default class First extends React.Component {
+  handleTextRef = ref => (this.text = ref);
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          animation={"bounceIn"}
+          duration={1200}
+          delay={200}
+          ref={ref => (this.logoImgRef = ref)}
+          style={styles.logoImg}
+          source={logo}
+        />
+        <Text style={styles.welcome}>Screen 1</Text>
+        <Button onPress={() => navigation.navigate("Second")}>
+          Go to screen ONE
+        </Button>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    backgroundColor: "#F5FCFF",
+    width: metrics.DEVICE_WIDTH,
+    height: metrics.DEVICE_HEIGHT,
+    paddingTop: 24
+  },
+  logoImg: {
+    flex: 1,
+    height: null,
+    width: IMAGE_WIDTH,
+    alignSelf: "center",
+    resizeMode: "contain",
+    marginVertical: 30
   },
   welcome: {
     fontSize: 20,
